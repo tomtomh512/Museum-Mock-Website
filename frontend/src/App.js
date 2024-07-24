0e1fbcbd429db3704750a76b19c08c09385246cf
thoma <thomashsu0512@gmail.com>
2024-07-24 03:35:38 -0400

    react, base code

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import Header from "./components/Header"
+import Exhibits from "./components/Exhibits"
+import Directory from "./components/Directory"
+import Shop from "./components/Shop"
+
+export default function App() {
+
+    const [mode, setMode] = useState("Exhibits")
+
+    const changeMode = (data) => setMode(data)
+
+    return (
+        <main>
+            <Header mode={mode} handleModeChange={changeMode}/>
+
+            {mode === "Exhibits" && <Exhibits />}
+            {mode === "Directory" && <Directory />}
+            {mode === "Shop" && <Shop />}
+
+        </main>
+    )
+}
