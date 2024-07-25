300bdde193efdcf1accd5142bcd6005eb927fdad
thoma <thomashsu0512@gmail.com>
2024-07-25 18:29:32 -0400

    updated exhibits page

--- frontend/src/components/Exhibits.js
+++ frontend/src/components/Exhibits.js
@@ -1,77 +1,29 @@
 import React from 'react'
-import fossils from '../images/fossils_1.png'
-import computers from '../images/computers.png'
-import ocean from '../images/ocean.png'
-import vivarium from '../images/vivarium.png'
+import exhibitsData from "../exhibitsData";
 import '../styles/Exhibits.css';
 
 export default function Exhibits(props) {
+
+    const exhibitElements = exhibitsData.map(item => {
+        return (
+            <section>
+                <div>
+                    <img src={item.photo} alt="fossils"/>
+                </div>
+                <div>
+                    <h2> {item.title} </h2>
+                    <hr/>
+                    <p> {item.description} </p>
+                </div>
+            </section>
+        )
+    })
 
     return (
         <main className="exhibit">
             <h1> Exhibits </h1>
             <hr/>
-
-            <section>
-                <div>
-                    <img src={fossils} alt="fossils"/>
-                </div>
-                <div>
-                    <h2> Fossil Collection </h2>
-                    <hr/>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. </p>
-                </div>
-            </section>
-
-            <br/>
-
-            <section>
-                <div>
-                    <h2> Underwater World </h2>
-                    <hr/>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. </p>
-                </div>
-                <div>
-                <img src={ocean} alt="computers"/>
-                </div>
-
-            </section>
-
-            <br/>
-
-            <section>
-                <div>
-                    <img src={vivarium} alt="ocean"/>
-                </div>
-                <div>
-                    <h2> Butterfly Vivarium </h2>
-                    <hr/>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. </p>
-                </div>
-            </section>
-
-            <br/>
-
-            <section>
-                <div>
-                    <h2> Digital World </h2>
-                    <hr/>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. </p>
-                </div>
-                <div>
-                <img src={computers} alt="fossils"/>
-                </div>
-
-            </section>
-
+            {exhibitElements}
         </main>
     )
 }
---
+++ frontend/src/exhibitsData.js
@@ -0,0 +1,35 @@
+import fossils from './images/fossils_1.png'
+import computers from './images/computers.png'
+import ocean from './images/ocean.png'
+import vivarium from './images/vivarium.png'
+
+export default [
+    {
+        photo: fossils,
+        title: "Fossil Collection",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n" +
+            "                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\n" +
+            "                        laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        photo: ocean,
+        title: "Underwater Wonders",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n" +
+            "                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\n" +
+            "                        laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        photo: vivarium,
+        title: "Butterfly Vivarium",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n" +
+            "                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\n" +
+            "                        laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        photo: computers,
+        title: "A New Digital World",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n" +
+            "                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\n" +
+            "                        laboris nisi ut aliquip ex ea commodo consequat."
+    }
+]
